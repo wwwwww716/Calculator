8541c466f9d7cec3f19999bb8d854b47b5ed5c19
m1 <wwwwww716@naver.com>
2023-04-06 08:14:37 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(2+4);
+        System.out.println(2*4);
+        System.out.println(4-3);
+        System.out.println(6/2);
+    }
+}
