ccddcd6cafd75126b30fa01226a1f0efc097e0ba
m1 <wwwwww716@naver.com>
2023-04-06 08:34:55 +0900

    사용자에게 2개의 숫자 값과 사칙연산 기호(+, -, *, /) 중 하나를 입력 받는다.

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,24 @@ public class Main {
         System.out.println("두번째 값 입력");
         int second = scanner.nextInt();
         System.out.println(second);
+        System.out.println();
 
-        System.out.println(first + second);
-        System.out.println(first - second);
-        System.out.println(first * second);
-        System.out.println(first / second);
+        System.out.println("사칙연산 기호 입력");
+        String str = scanner.next();
+        System.out.println(str);
+        // next 대신 nextLine 사용 시 자동으로 값이 입력됨
+
+        if (str.equals("+")) {
+            System.out.println(first + second);
+        } else if (str.equals("-")) {
+            System.out.println(first - second);
+        } else if (str.equals("*")) {
+            System.out.println(first * second);
+        } else if (str.equals("/")) {
+            System.out.println(first / second);
+        } else {
+            System.out.println("잘못된 기호를 입력하셨습니다.");
+        }
 
     }
 }
