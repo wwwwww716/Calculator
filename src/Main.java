a7479f3a64a58e8a949d39127f773bba0c314b93
m1 <wwwwww716@naver.com>
2023-04-06 09:05:58 +0900

    한 번에 2개의 숫자 값만 사칙연산을 하는 것이 아니라 여러 개의 사칙연산이 가능해야 한다. 앞 단계에서 계산한 결과 값은 이후 연산의 첫번째 값으로 사용해야 한다. 예를 들어 4 + 2 * 3의 순서로 값을 입력하면 먼저 4 + 2가 계산되고 결과 값 6과 3의 곱셈을 통해 18의 계산 결과가 나와야 한다. 사칙연산 기호 대신 "quit"이라는 문자열을 입력하면 최종 계산 결과를 출력하고 프로그램을 종료한다.

--- src/Main.java
+++ src/Main.java
@@ -7,27 +7,41 @@ public class Main {
         int first = scanner.nextInt();
         System.out.println(first);
 
-        System.out.println("두번째 값 입력");
-        int second = scanner.nextInt();
-        System.out.println(second);
-        System.out.println();
+        int result = 0;
 
-        System.out.println("사칙연산 기호 입력");
-        String str = scanner.next();
-        System.out.println(str);
-        // next 대신 nextLine 사용 시 자동으로 값이 입력됨
+        while(true) {
 
-        if (str.equals("+")) {
-            System.out.println(first + second);
-        } else if (str.equals("-")) {
-            System.out.println(first - second);
-        } else if (str.equals("*")) {
-            System.out.println(first * second);
-        } else if (str.equals("/")) {
-            System.out.println(first / second);
-        } else {
-            System.out.println("잘못된 기호를 입력하셨습니다.");
+            System.out.println("사칙연산 기호 입력");
+            String str = scanner.next();
+            System.out.println(str);
+
+            if (str.equals("quit")) {
+                System.out.println("계산기를 종료합니다");
+                break;
+            }
+
+            System.out.println("두번째 값 입력");
+            int second = scanner.nextInt();
+            System.out.println(second);
+
+            if (str.equals("+")) {
+                first = first + second;
+                System.out.println(first);
+            } else if (str.equals("-")) {
+                first = first - second;
+                System.out.println(first);
+            } else if (str.equals("*")) {
+                first = first * second;
+                System.out.println(first);
+            } else if (str.equals("/")) {
+                first = first / second;
+                System.out.println(first);
+            } else {
+                System.out.println("잘못된 기호를 입력하셨습니다.");
+            }
+
         }
+
 
     }
 }
