4db16566c3ef1df12a10b67f0a37b130f343ef93
m1 <wwwwww716@naver.com>
2023-04-06 10:47:31 +0900

    메서드 분리

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,64 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("첫번째 값 입력");
-        int first = scanner.nextInt();
-        System.out.println(first);
-
-        int result = 0;
+        int first = getFirstValue(scanner);
+        int result = first;
 
         while(true) {
+            String symbol = getSymbol(scanner);
 
-            System.out.println("사칙연산 기호 입력");
-            String str = scanner.next();
-            System.out.println(str);
-
-            if (str.equals("quit")) {
+            if (symbol.equals("quit")) {
+                System.out.println(result);
                 System.out.println("계산기를 종료합니다");
                 break;
             }
 
-            System.out.println("두번째 값 입력");
-            int second = scanner.nextInt();
-            System.out.println(second);
+            int second = getSecondValue(scanner);
 
-            if (str.equals("+")) {
-                first = first + second;
-                System.out.println(first);
-            } else if (str.equals("-")) {
-                first = first - second;
-                System.out.println(first);
-            } else if (str.equals("*")) {
-                first = first * second;
-                System.out.println(first);
-            } else if (str.equals("/")) {
-                first = first / second;
-                System.out.println(first);
-            } else {
-                System.out.println("잘못된 기호를 입력하셨습니다.");
-            }
+            result = calculate(result, symbol, second);
 
+            print(result);
         }
+     }
+     public static int getFirstValue(Scanner scanner) {
+         System.out.println("숫자를 입력해주세요");
+         int first = scanner.nextInt();
+         System.out.println("첫번째 숫자는 : " + first);
+         return first;
+     }
+     public static int getSecondValue(Scanner scanner) {
+        System.out.println("숫자를 입력해주세요");
+        int second = scanner.nextInt();
+        System.out.println("두번째 숫자는 : " + second);
+        return second;
+    }
+    public static String getSymbol(Scanner scanner) {
+        System.out.println("기호를 입력해주세요");
+        String symbol = scanner.next();
+        System.out.println("기호 : " + symbol);
+        return symbol;
+    }
 
+    public static int calculate (int first, String symbol, int second) {
+        int result = 0;
+        if (symbol.equals("+")) {
+            result = first + second;
+        } else if (symbol.equals("-")) {
+            result = first - second;
+        } else if (symbol.equals("*")) {
+            result = first * second;
+        } else if (symbol.equals("/")) {
+            result = first / second;
+        } else {
+            System.out.println("잘못된 기호를 입력했습니다");
+        }
+        return result;
+    }
 
+    public static void print(int result) {
+        System.out.println("결과는 : " + result);
     }
 }
